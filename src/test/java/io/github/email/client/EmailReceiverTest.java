35cee4544b6cebaa88cfc301d03396ff98148757
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-04-09 23:11:24 +0200

    Add sample test

---
+++ src/test/java/io/github/email/client/EmailReceiverTest.java
@@ -0,0 +1,13 @@
+package io.github.email.client;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmailReceiverTest {
+
+    @Test
+    void sampleTest() {
+        assertTrue(true);
+    }
+}
