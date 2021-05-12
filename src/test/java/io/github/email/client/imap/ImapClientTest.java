b851dd3b46be59acb56839d5a6cd077c56af4977
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-05-12 16:11:01 +0200

    Add imap anc smtp client simple tests.

---
+++ src/test/java/io/github/email/client/imap/ImapClientTest.java
@@ -0,0 +1,26 @@
+package io.github.email.client.imap;
+
+import io.github.email.client.service.ConfigService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class ImapClientTest {
+
+    private ImapClient imapClient;
+    private ConfigService configService;
+
+    @BeforeEach
+    void setUp() {
+        imapClient = new ImapClient(true);
+        configService = new ConfigService();
+    }
+
+    @Test
+    void shouldDownloadEmails() {
+        List<MailMetadata> mailMetadata = imapClient.downloadEmails(configService.getProperties(), 10);
+
+        System.out.println(mailMetadata.toString());
+    }
+}
