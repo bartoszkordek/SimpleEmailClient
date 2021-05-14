948bdb248a9129218ee2e62f32c27a95f012ece8
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-05-14 18:35:23 +0200

    Add ResponseDialogStage.

---
+++ src/main/java/io/github/email/client/ui/stages/ResponseDialogStage.java
@@ -0,0 +1,37 @@
+package io.github.email.client.ui.stages;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public class ResponseDialogStage extends Stage {
+
+    public ResponseDialogStage() {
+        super();
+        this.initModality(Modality.APPLICATION_MODAL);
+        this.setScene(getResponseScene());
+    }
+
+    private Scene getResponseScene() {
+        JFXButton button = new JFXButton();
+        button.getStyleClass().add("button-raised");
+        button.setText("OK");
+        button.setOnMouseClicked(event -> this.close());
+
+        Label label = new Label("Settings were saved successfully.");
+
+        VBox vBox = new VBox();
+        vBox.getChildren().add(label);
+        vBox.getChildren().add(button);
+
+        Scene scene = new Scene(vBox, 173, 100);
+        scene.getStylesheets()
+                .add(ResponseDialogStage.class.getResource("/css/components.css").toExternalForm());
+
+        return scene;
+    }
+
+}
