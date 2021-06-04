c383374f1374936580c3606db5fd5a51b013d5a6
Grzegorz Zacharski <g.zacharski@wp.pl>
2021-06-04 13:35:41 +0200

    Change ReceiveEmailGridPane - add EmailTextFields.

--- src/main/java/io/github/email/client/ui/components/gridpane/ReceiveEmailGridPane.java
+++ src/main/java/io/github/email/client/ui/components/gridpane/ReceiveEmailGridPane.java
@@ -1,27 +1,93 @@
 package io.github.email.client.ui.components.gridpane;
 
 import io.github.email.client.ui.components.textfields.CustomTextField;
-import io.github.email.client.ui.components.textfields.ReceivedEmailTextField;
+import io.github.email.client.ui.components.textfields.EmailTextField;
+import io.github.email.client.util.Email;
 import javafx.scene.layout.GridPane;
 
 public class ReceiveEmailGridPane extends GridPane {
 
-    public ReceiveEmailGridPane(ReceivedEmailTextField[] emailFields) {
+    public ReceiveEmailGridPane(Email email) {
         this.setVgap(10);
         this.setHgap(10);
         this.setStyle("-fx-padding: 10;");
-        this.buildGridPane(emailFields);
+        this.buildGridPane(email);
     }
 
-    private void buildGridPane(ReceivedEmailTextField[] emailFields) {
+    private void buildGridPane(Email email) {
+        EmailTextField[] emailFields = parseEmailAddressesFromEmail(email);
+
         for (int i = 0; i < emailFields.length; i++) {
             buildRowPane(emailFields[i], i);
         }
-        CustomTextField subjectField = new CustomTextField();
-        buildRowPane(subjectField, emailFields.length);
+    }
+
+    private EmailTextField[] parseEmailAddressesFromEmail(Email email) {
+
+        EmailTextField fromAddress = getFromAddresses(email);
+        EmailTextField ccAddresses = getCcAddresses(email);
+        EmailTextField bccAddresses = getBccAddresses(email);
+        EmailTextField subject = getSubjectEmail(email);
+
+        return new EmailTextField[]{
+                fromAddress,
+                ccAddresses,
+                bccAddresses,
+                subject
+        };
     }
 
     private void buildRowPane(CustomTextField field, int row) {
         this.add(field.getLabel(), 0, row);
+        this.add(field, 1, row);
     }
+
+    private EmailTextField getFromAddresses(Email email) {
+        EmailTextField fromAddresses = new EmailTextField("To:");
+
+        if (email.getFrom() == null) {
+            fromAddresses.setVisible(false);
+        } else {
+            fromAddresses.setText(email.getFrom());
+            fromAddresses.setEditable(false);
+        }
+        return fromAddresses;
+    }
+
+    private EmailTextField getCcAddresses(Email email) {
+        EmailTextField fromAddresses = new EmailTextField("Cc:");
+
+        if (email.getCc() == null) {
+            fromAddresses.setVisible(false);
+        } else {
+            fromAddresses.setText(email.getCc());
+            fromAddresses.setEditable(false);
+        }
+        return fromAddresses;
+    }
+
+    private EmailTextField getBccAddresses(Email email) {
+        EmailTextField fromAddresses = new EmailTextField("Bcc:");
+
+        if (email.getBcc() == null) {
+            fromAddresses.setVisible(false);
+        } else {
+            fromAddresses.setText(email.getBcc());
+            fromAddresses.setEditable(false);
+        }
+        return fromAddresses;
+    }
+
+    private EmailTextField getSubjectEmail(Email email) {
+        EmailTextField fromAddresses = new EmailTextField("Subject:");
+
+        if (email.getSubject() == null) {
+            fromAddresses.setVisible(false);
+        } else {
+            fromAddresses.setText(email.getSubject());
+            fromAddresses.setEditable(false);
+        }
+        return fromAddresses;
+    }
+
 }
